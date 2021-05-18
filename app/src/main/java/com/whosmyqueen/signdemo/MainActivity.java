fb1ba78633742d6517621d0e121bb75edb8a5315
郑志辉 <644049260@qq.com>
2021-05-18 17:49:01 +0800

    1. 升级SDK

--- app/src/main/java/com/whosmyqueen/signdemo/MainActivity.java
+++ app/src/main/java/com/whosmyqueen/signdemo/MainActivity.java
@@ -3,7 +3,7 @@ package com.whosmyqueen.signdemo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
